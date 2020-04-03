1bb8c6a68686c51a358a1f27a034b1d4d0a74180
Jing <62950141+go-visit@users.noreply.github.com>
2020-04-03 23:27:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
- var counter=0;
-function upvote(){counter=counter+1;
-document.getElementById("votes");
-.innerHTML=counter+" times";}
+var counter = 3;
+
+function upvote() {
+ counter = counter + 1;
+ 
+ document.getElementById("votes")
+  .innerHTML = counter + " votes";
+}
+
+function downvote() {
+ counter = counter - 1;
+ 
+ document.getElementById("votes")
+  .innerHTML = counter + " votes";
+}
