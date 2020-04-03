2f5981886e67a801e3733d08b8db19e624a869a4
Jing <62950141+lijing-homepage@users.noreply.github.com>
2020-04-03 19:55:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script>
 var part1 = "We're in here for ";
 var part2 = "JavaScript";
-console.log(part1 + part2);
+document.write(part1 + part2);
+</script>
+</body>
+</html>
