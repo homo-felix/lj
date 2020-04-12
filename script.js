fa9ef33b7bfdaaa0feba0411f38cac664c7245f8
Jing <62950141+homo-felix@users.noreply.github.com>
2020-04-13 01:15:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ var like = {};
 function likesF(item) {
  likesV.push(item);
  
- document.getElementById("likesP").innerHTML = likesV.length + " likes";
+ document.getElementById("likesP").innerHTML = likesV.length + " people have liked the page";
  return(likesV);
 }
-
 
